b14d2e9de8cf6ef421e268ca87e757bc12619ddc
it21062742 <it21062742@my.sliit.lk>
2022-10-23 19:06:16 +0530

    Added navigation between pages

--- app/src/main/java/com/example/blood/user_self_add_doc.java
+++ app/src/main/java/com/example/blood/user_self_add_doc.java
@@ -2,7 +2,10 @@ package com.example.blood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class user_self_add_doc extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class user_self_add_doc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_self_add_doc);
+
+        Button button =findViewById(R.id.pharmacy_btn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),user_self_add_pharmacy.class);
+                startActivity(i);
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getApplicationContext(),HomePage.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/blood/user_self_add_pharmacy.java
+++ app/src/main/java/com/example/blood/user_self_add_pharmacy.java
@@ -2,7 +2,10 @@ package com.example.blood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class user_self_add_pharmacy extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class user_self_add_pharmacy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_self_add_pharmacy);
+
+        Button button =findViewById(R.id.doc_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                overridePendingTransition(0, 0 );
+                Intent i = new Intent(getApplicationContext(),user_self_add_doc.class);
+                startActivity(i);
+            }
+        });
+
+    }
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getApplicationContext(),HomePage.class);
+        startActivity(i);
     }
 }
