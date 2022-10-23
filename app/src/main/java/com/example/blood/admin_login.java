33c851ac1d52d185590e6bc46c4bb1cd8097be8f
it21062742 <it21062742@my.sliit.lk>
2022-10-24 00:42:08 +0530

    Added sign in page and created the db

--- app/src/main/java/com/example/blood/admin_login.java
+++ app/src/main/java/com/example/blood/admin_login.java
@@ -2,13 +2,64 @@ package com.example.blood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Database.Login;
+
 
 public class admin_login extends AppCompatActivity {
-
+        EditText username, password;
+        Button login;
+        Login DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        username = (EditText) findViewById(R.id.EmailAddress);
+        password = (EditText) findViewById(R.id.Password);
+        login = (Button) findViewById(R.id.sign_up_btn);
+        DB = new Login(this);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("admin@blueblood.com") && pass.equals("Bleeding@123")){
+                    Toast.makeText(admin_login.this, "Admin login successful", Toast.LENGTH_SHORT).show();
+                    Intent intent  = new Intent(getApplicationContext(), admin_home.class);
+                    startActivity(intent);}
+                if(user.equals("") && pass.equals(""))
+                    Toast.makeText(admin_login.this, "Please fill the user name or password", Toast.LENGTH_LONG).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(admin_login.this, "Sign in successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent1  = new Intent(getApplicationContext(), HomePage.class);
+                        startActivity(intent1);
+                    }else{
+                        Toast.makeText(admin_login.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+            Button button1 =findViewById(R.id.Btn_signUp);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),admin_register.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/blood/admin_register.java
@@ -0,0 +1,68 @@
+package com.example.blood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Database.Login;
+
+public class admin_register extends AppCompatActivity {
+    EditText username, password, repassword;
+    Button signup;
+    Login DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_register);
+
+        username = (EditText) findViewById(R.id.EmailAddress);
+        password = (EditText) findViewById(R.id.Password);
+        repassword = (EditText) findViewById(R.id.Password2);
+        signup = (Button) findViewById(R.id.sign_up_btn);
+        DB = new Login(this);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(admin_register.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert==true){
+                                Toast.makeText(admin_register.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),admin_login.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(admin_register.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(admin_register.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(admin_register.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    }
+                } }
+        });
+
+        Button button1 =findViewById(R.id.btn_signIn);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),admin_login.class);
+                startActivity(i);
+            }
+        });
+    }
+}
