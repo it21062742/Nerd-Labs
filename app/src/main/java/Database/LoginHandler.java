32d07045e822751642708e7cc5533dc40a60763d
it21062742 <it21062742@my.sliit.lk>
2022-10-24 19:30:44 +0530

    Update LoginHandler.java

--- app/src/main/java/Database/LoginHandler.java
+++ app/src/main/java/Database/LoginHandler.java
@@ -13,12 +13,12 @@ public class LoginHandler extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
-        MyDB.execSQL("create Table user(username TEXT primary key, password TEXT)");
+        MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
-        MyDB.execSQL("drop Table if exists user");
+        MyDB.execSQL("drop Table if exists users");
     }
 
     public Boolean insertData(String username, String password){
@@ -26,7 +26,7 @@ public class LoginHandler extends SQLiteOpenHelper {
         ContentValues contentValues= new ContentValues();
         contentValues.put("username", username);
         contentValues.put("password", password);
-        long result = MyDB.insert("user", null, contentValues);
+        long result = MyDB.insert("users", null, contentValues);
         if(result==-1) return false;
         else
             return true;
@@ -34,7 +34,7 @@ public class LoginHandler extends SQLiteOpenHelper {
 
     public Boolean checkusername(String username) {
         SQLiteDatabase MyDB = this.getWritableDatabase();
-        Cursor cursor = MyDB.rawQuery("Select * from user where username = ?", new String[]{username});
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ?", new String[]{username});
         if (cursor.getCount() > 0)
             return true;
         else
@@ -43,7 +43,7 @@ public class LoginHandler extends SQLiteOpenHelper {
 
     public Boolean checkusernamepassword(String username, String password){
         SQLiteDatabase MyDB = this.getWritableDatabase();
-        Cursor cursor = MyDB.rawQuery("Select * from user where username = ? and password = ?", new String[] {username,password});
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ? and password = ?", new String[] {username,password});
         if(cursor.getCount()>0)
             return true;
         else
