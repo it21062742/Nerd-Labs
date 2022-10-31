0d0d6cc0eaba297410886c59e77b2a2a622c31ce
it21062742 <it21062742@my.sliit.lk>
2022-10-31 21:55:25 +0530

    Update admin_add_doc_brief.java

--- app/src/main/java/com/example/blood/admin_add_doc_brief.java
+++ app/src/main/java/com/example/blood/admin_add_doc_brief.java
@@ -3,11 +3,7 @@ package com.example.blood;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,45 +13,46 @@ import java.util.ArrayList;
 
 import Database.CurrentReqHandler;
 import Database.CurrentUser;
-import Database.DeliveryReqHandler;
 import Database.DeliveryReqTable;
 import Database.DoctorHandler;
 
 public class admin_add_doc_brief extends AppCompatActivity {
-   DoctorHandler myDB;
-   ArrayList<String> id, name, email, contact, hospital, Qualification;
-   RecyclerView recyclerView;
-   AdapterFor_doc custAdapter;
+    DoctorHandler myDB;
+    ArrayList<String> id, name, email, contact, hospital, Qualification;
+    RecyclerView recyclerView;
+    AdapterFor_doc custAdapter;
     String email1;
-   @Override
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_add_doctor_brief);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_doctor_brief);
 
-       recyclerView = (RecyclerView) findViewById(R.id.DocView);
+        recyclerView = (RecyclerView) findViewById(R.id.DocView);
 
-       id = new ArrayList<String>();
-       name = new ArrayList<String>();
-       email = new ArrayList<String>();
-       contact = new ArrayList<String>();
-       hospital = new ArrayList<String>();
+        id = new ArrayList<String>();
+        name = new ArrayList<String>();
+        email = new ArrayList<String>();
+        contact = new ArrayList<String>();
+        hospital = new ArrayList<String>();
 
-       DoctorHandler dh = new DoctorHandler(this, DeliveryReqTable.DeliveryReq.TABLENAME, null, 1);
-       CurrentReqHandler currentReqHandler = new CurrentReqHandler(this, CurrentUser.PresentUser.TABLENAME, null, 1);
-       Cursor cursor1 = currentReqHandler.getUser();
+        DoctorHandler dh = new DoctorHandler(this, DeliveryReqTable.DeliveryReq.TABLENAME, null, 1);
+        CurrentReqHandler currentReqHandler = new CurrentReqHandler(this, CurrentUser.PresentUser.TABLENAME, null, 1);
+        Cursor cursor1 = currentReqHandler.getUser();
 
-       cursor1.moveToNext();
+        cursor1.moveToNext();
 
-       fetchRecords1();
-       custAdapter = new AdapterFor_doc(admin_add_doc_brief.this,this, id,name,email,contact,hospital,Qualification);
-       recyclerView.setAdapter(custAdapter);
-       recyclerView.setLayoutManager(new LinearLayoutManager(admin_add_doc_brief.this));
-   }
+        fetchRecords1();
+        custAdapter = new AdapterFor_doc(admin_add_doc_brief.this, this, id, name, email, contact, hospital, Qualification);
+        recyclerView.setAdapter(custAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(admin_add_doc_brief.this));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==1)
+        if (requestCode == 1)
             recreate();
     }
 
@@ -63,7 +60,7 @@ public class admin_add_doc_brief extends AppCompatActivity {
         DoctorHandler dh = new DoctorHandler(admin_add_doc_brief.this, DoctorHandler.TABLENAME, null, 1);
         Cursor cursor = dh.readData();
 
-        if (cursor!=null && cursor.getCount()>0) {
+        if (cursor != null && cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
                 name.add(cursor.getString(1));
                 email.add(cursor.getString(2));
@@ -73,20 +70,4 @@ public class admin_add_doc_brief extends AppCompatActivity {
             }
         }
     }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.completed_pharm_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.Completed) {
-            Intent i = new Intent(getApplicationContext(), PharmacyCompleted.class);
-            startActivity(i);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
